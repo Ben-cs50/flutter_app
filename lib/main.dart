8e75ce4f0eecdf775bbd7b6ab1a54a28dd48b408
Ben-cs50 <benedictodhiambo49@gmail.com>
2026-08-18 09:12:31 +0300

    add hamburger drawer

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,46 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Expense Tracker'),
-          backgroundColor: const Color(0xFF1A1A24) ,//fromARGB(222, 26, 223, 125),
+          title: const Text('Expense Tracker'),
+          backgroundColor: const Color(0xFF1A1A24),
         ),
-
-        body: Column(
-          
-          children: [
-            SizedBox(height: 10.0),
-            Align(
-              alignment: Alignment.centerRight,
-              child: ClipOval(
-              
-                child: Image.network(
-                  height: 50,
-                  fit: BoxFit.cover,
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRllE5B1hwxmrgl_Mof16Y7N3axkoBdjAsTUfCZ3DSeww&s=10',
+        
+        
+        drawer: Drawer(
+          child: SafeArea( 
+            child: Column(
+              children: [
+                const SizedBox(height: 10.0),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 16.0), 
+                    child: ClipOval(
+                      child: Image.network(
+                        height: 50,
+                        width: 50, 
+                        fit: BoxFit.cover,
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRllE5B1hwxmrgl_Mof16Y7N3axkoBdjAsTUfCZ3DSeww&s=10',
+                      ),
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(height: 10.0),
+                MenuItem(icon: Icons.home, title: 'Dashboard', onTap: () {}),
+                const SizedBox(height: 10.0),
+                MenuItem(title: 'Transactions', icon: Icons.receipt, onTap: () {}),
+                const SizedBox(height: 10.0),
+                MenuItem(title: 'Analytics', icon: Icons.analytics, onTap: () {}),
+                const SizedBox(height: 10.0),
+                MenuItem(title: 'Budgets', icon: Icons.account_balance, onTap: () {}),
+              ],
             ),
-            SizedBox(height: 10.0),
-            MenuItem(icon: Icons.home, title: 'Dashboard', onTap: () {}, ),
-            SizedBox(height: 10.0),
-            MenuItem(title: 'Transactions', icon: Icons.receipt, onTap: () {},),
-            SizedBox(height: 10.0),
-            MenuItem(title: 'Analytics', icon: Icons.analytics, onTap: () {},),
-            SizedBox(height: 10.0),
-            MenuItem(title: 'Budgets', icon: Icons.account_balance, onTap: () {},),
-          ],
+          ),
+        ),
+        
+        
+        body: const Center(
+          child: Text('Main Content Area (Dashboard, etc.)'),
         ),
       ),
     );
